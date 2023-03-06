8756478b16fdc6d3012aa525f34ac5a381586cec
MinJun Choi <mj043000@naver.com>
2023-03-06 10:34:04 +0900

    Add: BOJ10986 Main

---
+++ data_structure/10986/Main.java
@@ -0,0 +1,31 @@
+// Created on iPhone.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        long[] S = new long[N];
+        long[] C = new long[M];
+        long answer = 0;
+
+        S[0] = sc.nextInt();
+        for (int i = 1; i<N; i++) {
+            S[i] = S[i-1] + sc.nextInt();
+        }
+
+		for (int i = 0; i<N; i++) {
+            int remainder = (int)(S[i]%M);
+            if (remainder==0) answer++;
+            C[remainder]++;
+        }
+
+        for (int i=0; i<M; i++) {
+            if(C[i]>1) {
+                answer = answer + (C[i] * (C[i]-1) / 2);
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
