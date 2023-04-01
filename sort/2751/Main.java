210607f20f733a27c887f5e03ad9d479c056c1b3
MinJun Choi <mj043000@naver.com>
2023-04-01 22:18:25 +0900

    Fix: update BOJ2751 Main.java

--- sort/2751/Main.java
+++ sort/2751/Main.java
@@ -1,20 +1,32 @@
-public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int N = Integer.parseInt(br.readLine());
-    StringBuilder sb = new StringBuilder();
-	
-    // 리스트에 숫자넣기
-    ArrayList<Integer> list = new ArrayList<>();
-    for(int i = 0; i < N; i++) {
-      list.add(Integer.parseInt(br.readLine()));
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException
+    {
+        int n = readInt();
+        boolean[] arr = new boolean[2000001];
+        for (int i = 0; i < n; i++)
+            arr[readInt() + 1000000] = true;
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i])
+                sb.append(i - 1000000).append('\n');
+        
+        System.out.print(sb);
     }
-	
-    // 오름차순 정렬
-    Collections.sort(list);
-	
-    // 출력
-    for(int value : list) {
-      sb.append(value).append('\n');
+    
+    static int readInt() throws IOException {
+        int n = 0;
+        boolean isNegative = false;
+        while (true) {
+            int input = System.in.read();
+            if (input <= 32) {
+                return isNegative ? n * -1 : n;
+            } else if (input == '-')
+                isNegative = true;
+            else
+                n = (n << 3) + (n << 1) + (input & 15);
+        }
     }
-    System.out.println(sb);
 }
